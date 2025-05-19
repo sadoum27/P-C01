8367db549c4b16bcf7db788bec4c3e9adf595ce1
sadoum27 <manousadou57@gmail.com>
2025-05-19 01:13:57 +0100

    Update find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -2,18 +2,13 @@
 
 # Script : find_string.sh
 # Objectif : Rechercher une chaîne spécifique dans un fichier
-# Utilise : grep
-# Auteur : [Votre nom]
-# Date : [Date]
 
-read -p "Nom du fichier : " filename
-
+read -p "Nom du fichier :"filename
 if [ ! -f "$filename" ]; then
     echo "Erreur : le fichier '$filename' n'existe pas."
     exit 1
 fi
-
-read -p "Chaîne à rechercher : " search
+read -p "Chaîne à rechercher :"search
 
 if grep -q "$search" "$filename"; then
     echo "La chaîne '$search' a été trouvée dans $filename."
